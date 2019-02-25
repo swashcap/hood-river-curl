6d15ca2c974699674c15c693e3b62ff529ea4030
Cory Reed <swashcap@gmail.com>
2019-02-25 01:34:25 -0800

    fix: correct backslash in filename

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ mkdir -p images
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 curl https://tripcheck.com/RoadCams/cams/Hood%20River%20Exit%2063_pid1860.JPG \
-  -o "$PWD/images/$DATE\_63.jpeg" &
+  -o "$PWD/images/${DATE}_63.jpeg" &
 
 curl https://tripcheck.com/RoadCams/cams/Hood%20River%20Exit%2064a_pid1863.JPG \
-  -o "$PWD/images/$DATE\_64A.jpeg"
+  -o "$PWD/images/${DATE}_64A.jpeg"
 
 wait
